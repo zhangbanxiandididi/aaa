ddffa93492391d7c358ff8d5e9737aa10f161e5c
aa <aa@163.com>
2022-03-01 15:40:06 +0800

    tt

--- test.py
+++ test.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python
 print("hello")
 print("hello")
-print("hello")
-print("hello")
-print("hello")
-print("hello")
-print("hello")
-print("hello")
-print("hello")
+print("123")
+print("123")
+print("123")
